3058c2e59a5e5b05d0f7e8be2dd6ca4efcd42389
jese1007 <82769161+jese1007@users.noreply.github.com>
2023-03-15 15:30:52 +0300

    first commit

---
+++ src/main/java/mg/itu/rakotonandrasana/tp1/ejb/CustomerManager.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.rakotonandrasana.tp1.ejb;
+
+import mg.itu.rakotonandrasana.tp1.entite.Customer;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import java.util.List;
+
+/**
+ *
+ * @author dajes
+ */
+@Stateless
+public class CustomerManager {
+
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+
+    public void persist(Customer customer) {
+        em.persist(customer);
+    }
+
+    public List<Customer> getAllCustomers() {
+        Query query = em.createNamedQuery("Customer.findAll");
+        return query.getResultList();
+    }
+
+    public Customer update(Customer customer) {
+        return em.merge(customer);
+    }
+}
