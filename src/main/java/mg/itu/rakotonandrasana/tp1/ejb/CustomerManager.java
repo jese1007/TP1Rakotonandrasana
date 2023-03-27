be4ec5fb9a84d15c4aaf07e1671ef5cda927db39
jese1007 <82769161+jese1007@users.noreply.github.com>
2023-03-27 14:44:27 +0300

    final tp 1

--- src/main/java/mg/itu/rakotonandrasana/tp1/ejb/CustomerManager.java
+++ src/main/java/mg/itu/rakotonandrasana/tp1/ejb/CustomerManager.java
@@ -33,4 +33,8 @@ public class CustomerManager {
     public Customer update(Customer customer) {
         return em.merge(customer);
     }
+    
+    public Customer findById(int idCustomer) {
+        return em.find(Customer.class, idCustomer);
+    }
 }
